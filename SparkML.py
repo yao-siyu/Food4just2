87ce0595a5e007856328e14f1d973c27e86d4d26
SIYU YAO <siyu@SIYUs-MacBook-Air.local>
2019-02-14 22:31:56 -0800

    Add the updated version

--- SparkML.py
+++ SparkML.py
@@ -1,11 +1,14 @@
-# Build the recommendation model using Alternating Least Squares (ALS)
 from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
+from pyspark import StorageLevel
+import yaml
+
+config = yaml.load(open('../config.yaml'))
 
 newratingsRDD = newratings.rdd
 ratingsML = newratingsRDD.map(lambda l: Rating(int(l[6]), int(l[8]), float(l[3])))
 
-rank = 10
-numIterations = 10
+rank = 100
+numIterations = 20
 model = ALS.train(ratingsML, rank, numIterations)
 
 # Evaluate the model on training data
@@ -17,8 +20,18 @@ MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
 print("Mean Squared Error = " + str(MSE))
 
 #0.327520844251 -- rank = 10, numIterations = 10
-
+#0.0835593328909 -- rank = 50, numIterations = 10
 
 # Save and load model
-model.save(sc, "s3n://restaurant-picker-data/model")
-sameModel = MatrixFactorizationModel.load(sc, "s3n://restaurant-picker-data/model")
+
+model.save(sc, config['S3_model'])
+
+model = MatrixFactorizationModel.load(sc, config['S3_model'])
+
+userFeatures = model.userFeatures().repartition(1)
+userFeatures.persist(StorageLevel.MEMORY_AND_DISK_SER)
+
+productFeatures = model.productFeatures().repartition(1)
+productFeatures.persist(StorageLevel.MEMORY_AND_DISK_SER)
+
+predictions = model.predictAll(user_res.rdd).map(lambda r: ((r[0], r[1]), r[2]))
